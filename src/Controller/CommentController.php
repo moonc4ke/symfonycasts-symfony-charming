e9ba7b340acd570bf449910f9c3d956302f0f2e8
semicolon <adom.donatas@gmail.com>
2021-12-15 20:23:52 +0200

    chapter 15.

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+    /**
+     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
+     */
+    public function commentVote($id, $direction)
+    {
+        // TODO use id to query database
+
+        // use real logic here to save this to the database
+        if ($direction === 'up') {
+            $currentVoteCount = rand(7, 100);
+        } else {
+            $currentVoteCount = rand(0, 5);
+        }
+
+        return $this->json(['votes' => $currentVoteCount]);
+    }
+}
